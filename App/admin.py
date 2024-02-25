2b47927bcbbe077b9734afc400674b09a4ac8c46
Иванов Глеб Алексеевич <glebaiva@gmail.com>
2024-02-25 11:56:23 +0300

    Add DataBase V.0 and admin panel

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from App.models import Ingredients, Equipment, Replacement, Likes, Cocktail
+
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Likes)
+class LikesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Replacement)
+class ReplacementAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cocktail)
+class CocktailAdmin(admin.ModelAdmin):
+    pass
--- App/models.py
+++ App/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=48)
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=48)
+
+
+class Cocktail(models.Model):
+    name = models.CharField(max_length=48)
+    description = models.CharField(max_length=128)
+    equipment = models.ManyToManyField(Equipment)
+    ingredients = models.ManyToManyField(Ingredients)
+    alcohol = models.BooleanField()
+
+
+class Replacement(models.Model):
+    class Meta:
+        unique_together = (('id_cocktail', 'id_ingredient', 'id_replacement'),)
+    id_cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+    id_ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE, related_name="ingredient")
+    id_replacement = models.ForeignKey(Ingredients, on_delete=models.CASCADE, related_name="replacement")
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+    like = models.BooleanField(default=None)
